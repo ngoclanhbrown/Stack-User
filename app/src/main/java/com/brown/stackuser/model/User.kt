d50e02f41f7fc9aba05fbd195837b6a8803b8b25
ngoclanhbrown <tnlanh99@gmail>
2020-03-12 16:34:18 +0700

    Add User data class

---
+++ app/src/main/java/com/brown/stackuser/model/User.kt
@@ -0,0 +1,24 @@
+package com.brown.stackuser.model
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+/**
+ * Immutable model class for a StackOverflow user that holds all the information about a user.
+ * This class defines the Room repos table, where the repo [id] is the primary key.
+ * It is also be parcelized to be able to send via Bundle
+ */
+@Entity
+@Parcelize
+data class User(
+    @PrimaryKey
+    val id: Long,
+    val name: String,
+    val reputation: Int,
+    val location: String,
+    val profileImageUrl: String,
+    val lastAccessDate: Long,
+    val favorite: Boolean
+) : Parcelable
