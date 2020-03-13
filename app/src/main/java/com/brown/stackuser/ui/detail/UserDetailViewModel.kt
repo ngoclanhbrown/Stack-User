0e247f3a4484fe83b334b3c3edce0e5dccf89011
ngoclanhbrown <tnlanh99@gmail>
2020-03-13 14:06:59 +0700

    Add user field to UserDetailViewModel

--- app/src/main/java/com/brown/stackuser/ui/detail/UserDetailViewModel.kt
+++ app/src/main/java/com/brown/stackuser/ui/detail/UserDetailViewModel.kt
@@ -1,7 +1,23 @@
 package com.brown.stackuser.ui.detail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.brown.stackuser.model.User
 
-class UserDetailViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class UserDetailViewModel(
+    val user: User
+) : ViewModel() {
+
+
+    class Factory(
+        private val user: User
+    ) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(UserDetailViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return UserDetailViewModel(user) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
 }
