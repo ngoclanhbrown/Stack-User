2728642fc74d435a4ad059da1c4a294dc8467dab
ngoclanhbrown <tnlanh99@gmail>
2020-03-13 11:36:02 +0700

    Update UserListViewModel: - Change users from LiveData to MediatorLiveData - Add currentFilter property - Add updateFilter method - Add updateFavoriteUser method

--- app/src/main/java/com/brown/stackuser/ui/overview/UserListViewModel.kt
+++ app/src/main/java/com/brown/stackuser/ui/overview/UserListViewModel.kt
@@ -1,14 +1,57 @@
 package com.brown.stackuser.ui.overview
 
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.paging.PagedList
+import com.brown.stackuser.model.User
+import com.brown.stackuser.model.UserFilter
 import com.brown.stackuser.repository.UserRepository
 
 class UserListViewModel(
     private val repository: UserRepository
 ) : ViewModel() {
 
-    val users = repository.users
+    private val allUsers = repository.allUsers
+    private val favoriteUsers = repository.favoriteUsers
+
+    val users = MediatorLiveData<PagedList<User>>()
+
+    private var currentFilter = UserFilter.ALL
+
+    init {
+        users.addSource(allUsers) {
+            if (currentFilter == UserFilter.ALL) {
+                it?.let {
+                    users.value = it
+                }
+            }
+        }
+
+        users.addSource(favoriteUsers) {
+            if (currentFilter == UserFilter.FAVORITE) {
+                it?.let {
+                    users.value = it
+                }
+            }
+        }
+    }
+
+
+    fun updateFavoriteUser(user: User) {
+        repository.updateFavoriteUser(user.id, !user.favorite)
+    }
+
+
+    fun updateFilter(newFilter: UserFilter) {
+        if (currentFilter != newFilter) {
+            when (newFilter) {
+                UserFilter.ALL -> allUsers.value?.let { users.value = it }
+                UserFilter.FAVORITE -> favoriteUsers.value?.let { users.value = it }
+            }
+            currentFilter = newFilter
+        }
+    }
 
 
     class Factory(
