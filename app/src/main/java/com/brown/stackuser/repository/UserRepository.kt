6ee43893d16562b2ddce332ba49e5701c56fcac9
ngoclanhbrown <tnlanh99@gmail>
2020-03-13 09:54:08 +0700

    Add users field to repo

--- app/src/main/java/com/brown/stackuser/repository/UserRepository.kt
+++ app/src/main/java/com/brown/stackuser/repository/UserRepository.kt
@@ -1,12 +1,24 @@
 package com.brown.stackuser.repository
 
+import androidx.paging.toLiveData
 import com.brown.stackuser.api.StackOverflowService
 import com.brown.stackuser.database.LocalCache
+import java.util.concurrent.Executor
 
 class UserRepository(
     private val service: StackOverflowService,
-    private val cache: LocalCache
+    private val cache: LocalCache,
+    private val executor: Executor
 ) {
 
+    companion object {
+        const val DATABASE_PAGE_SIZE = 25
+    }
+
+    val users = cache.getUsers().toLiveData(
+        DATABASE_PAGE_SIZE,
+        boundaryCallback = UserBoundaryCallback(service, cache),
+        fetchExecutor = executor
+    )
 
 }
